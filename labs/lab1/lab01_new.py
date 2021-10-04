c432f16408b2518e6ef2e6e9a0cf2bd6cbbd4e77
YanaShershan <yanashershen90@gmail.com>
2021-10-04 18:17:46 +0300

    add lab01_new

---
+++ labs/lab1/lab01_new.py
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+a = 5
+b = 6
+c = 100.0
+d = "Hello world!"
+e = [a,b,c,(a+b/c),d]
+print(f'List example: {e}')
+print(f'Allowed opertion exmple: {a+b+c}')
+# print(f'Result type checking error example {b+d}')
+=======
+
+>>>>>>> 2f82ab06057f9722e8b18e35cfdd4ebf341e1ba2
