6c64c22e8db880b9f92038fb17906a05e69b30ed
YanaShershan <yanashershen90@gmail.com>
2021-10-04 18:09:19 +0300

    lab01_basics.py file has been added

---
+++ labs/lab1/lab01_basics.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+c = 100.0
+d = "Hello world!"
+e = [a,b,c,(a+b/c),d]
+print(f'List example: {e}')
+print(f'Allowed opertion exmple: {a+b+c}')
+# print(f'Result type checking error example {b+d}')
